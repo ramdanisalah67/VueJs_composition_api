f6b5763145b50d7e7dbb7cccc8f3b138ec848b8e
ramdanisalah67 <ramdanisalah67@gmail.com>
2024-05-06 14:27:30 +0100

    composition api => emits,custom directive

---
+++ src/components/directiveComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="body">
+    <div v-bg="'black'">Welcome,salah-eddine to directive</div>
+  </div>
+</template>
+<script setup>
+import { useRoute, useRouter } from "vue-router"
+
+//custom directive 
+
+const vBg = {
+    mounted : (el,data) => {
+        console.log("element => ",el)
+        console.log("data => ",data)
+        el.style.background = data.value
+        el.style.color = "white"
+        el.style.padding = "10px"
+        el.style.textAlign = "center"
+        el.style.borderRadius = "10px"
+        el.style.boxShadow = "0px 0px 10px 0px rgba(0,0,0,0.5)"
+    }
+}
+
+
+const router = useRouter()
+console.log("Router from view => ", router)
+
+const route = useRoute()
+console.log("Route from view => ", route)
+</script>
---
+++ src/components/emitTest.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <p>this is emit event test</p>
+        <button @click="emits('handleClick')">login</button>
+    </div>
+</template>
+<script setup>
+import {defineEmits} from 'vue'
+const emits = defineEmits(['handleClick'])
+</script>
